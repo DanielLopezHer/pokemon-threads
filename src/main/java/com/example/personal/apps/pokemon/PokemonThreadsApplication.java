1ec85ad13d29b73565dec4c2771a9b5ae5817af1
DanielLopezHer <daniel.dhernandez@mercadolibre.com.mx>
2022-03-21 17:57:02 -0600

    Commit inicial, se agregan proyecto de spring initializr

---
+++ src/main/java/com/example/personal/apps/pokemon/PokemonThreadsApplication.java
@@ -0,0 +1,13 @@
+package com.example.personal.apps.pokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonThreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonThreadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/personal/apps/pokemon/PokemonThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personal.apps.pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonThreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
